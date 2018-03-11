4168fde21fe7e0468a69cef44149ead9b380729f
Duc Quan <ducquan7850@gmail.com>
2018-03-12 00:35:37 +0700

    s commit

---
+++ Cau4/src/main/java/com/mycompany/cau4/GuavaCache.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cau4;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author DUC QUAN
+ */
+public class GuavaCache {
+    private static LoadingCache<Integer, String> cache;
+    static {
+        cache = CacheBuilder.newBuilder()
+           .maximumSize(100)
+           .expireAfterWrite(10, TimeUnit.MINUTES)
+           .build(
+                new CacheLoader<Integer, String>() {
+                    @Override
+                    public String load(Integer id) throws Exception {
+                            return getStringById(id);
+                    }
+                }
+        );
+    }
+    public static LoadingCache<Integer, String> getLoadingCache() {
+	return cache;
+    }
+    public static String getStringById(int id) {
+        return "";
+    }
+}
---
+++ Cau4/src/main/java/com/mycompany/cau4/NewClass.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cau4;
+
+import com.google.common.cache.LoadingCache;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import static jdk.nashorn.internal.objects.NativeFunction.function;
+
+/**
+ *
+ * @author DUC QUAN
+ */
+public class NewClass {
+    public static String KQ(int k){
+        String s="";
+        for(int i=1;i<k;i++){
+            s=s+i+" ";
+        }
+        return s;
+    }
+    public static void main(String[] args) throws ExecutionException {
+        LoadingCache<Integer, String> c = GuavaCache.getLoadingCache();
+        Scanner in=new Scanner(System.in);
+            long lancuoi=System.currentTimeMillis();
+        while(true){
+            System.out.println("Nhap so: ");
+            int k=in.nextInt();
+            long lantiep=System.currentTimeMillis();
+            c.put(k, KQ(k));
+            if(lantiep-lancuoi>=10000){
+                System.out.println("Da qua 10s, cache bi xoa!");
+                c.invalidateAll();
+            }
+            System.out.println(c.get(k));
+            System.out.println("Size= "+c.size());
+            lancuoi=System.currentTimeMillis();
+            if(k==-1)break;
+        }
+    }
+}
